9f77f7985d5cf45fd574c0cae3a881f68404995d
Alexander Malmberg <alexander@malmberg.org>
2002-06-27 17:02:35 +0000

    Try to improve initial resizing.

--- TerminalParser_Linux.m
+++ TerminalParser_Linux.m
@@ -1107,7 +1107,7 @@ static unsigned char color_table[] = { 0, 4, 2, 6, 1, 5, 3, 7,
 
 -(void) setTerminalScreenWidth: (int)w height: (int)h
 {
-	x+=w-width;
+//	x+=w-width;
 	y+=h-height;
 
 	width=w;
