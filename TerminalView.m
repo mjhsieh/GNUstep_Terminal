5fb08677ee998a24f63d09172d954456f6ac6aa3
Alexander Malmberg <alexander@malmberg.org>
2002-08-19 13:35:16 +0000

    Add GPL notices.

--- TerminalView.m
+++ TerminalView.m
@@ -1,5 +1,10 @@
 /*
 copyright 2002 Alexander Malmberg <alexander@malmberg.org>
+
+This file is a part of Terminal.app. Terminal.app is free software; you
+can redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation; version 2
+of the License. See COPYING or main.m for more information.
 */
 
 /*
